afd41d0309ece27b1f53e899fd7ed46797cea795
jack <lovekhj@gmail.com>
2026-02-02 11:37:15 +0900

    init: ipms project

---
+++ src/test/java/com/ipmsprj/ipms/IpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipmsprj.ipms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
