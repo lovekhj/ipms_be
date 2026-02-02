d379c94980a1bbac8d4d0130b73d878393106b9f
jack <lovekhj@gmail.com>
2026-02-02 21:46:33 +0900

    프로젝트리스트 화면수정

--- src/main/java/com/ipmsprj/ipms/prj/dto/PrjDto.java
+++ src/main/java/com/ipmsprj/ipms/prj/dto/PrjDto.java
@@ -4,13 +4,18 @@ import lombok.Data;
 
 @Data
 public class PrjDto {
+    private String rowNum;
     private String prjId;
     private String prjNm;
     private String prjGbn;
+    private String prjGbnNm;
     private String prjStrDt;
     private String prjEndDt;
     private String prjStsCd;
+    private String prjStsNm;
     private String prjDesc;
     private String officeLoc;
+    private String officeLocNm;
     private String bizPrtnr;
+    private String bizPrtnrNm;
 }
